d16cc531408371dce5ce04f888f24e8072e4e4bd
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-09-06 03:11:47 +0300

    Edit application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
 
   include Knock::Authenticable
+
+  def user_id
+    if decoded_token
+      decoded_token[0]["user_id"]
+    end
+  end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://guarded-refuge-24371.herokuapp.com'
 
     resource '*',
       headers: :any,
