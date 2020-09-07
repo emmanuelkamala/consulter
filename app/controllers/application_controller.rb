09a577f4693a6976c327cbe273fb8c0ce6692268
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-09-07 10:24:34 +0300

    Remove unnecessary files in the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::API
 
   include Knock::Authenticable
 
-  def user_id
-    if decoded_token
-      decoded_token[0]["user_id"]
-    end
-  end
+  # def user_id
+  #   if decoded_token
+  #     decoded_token[0]["user_id"]
+  #   end
+  # end
 end
