7a0d96663f5b76972b0b2dc14474de167aca5ab3
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-09-08 00:39:29 +0300

    Edit user token controller

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,3 +1,5 @@
 class UserTokenController < Knock::AuthTokenController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
+  protect_from_forgery except: :create
+
 end
