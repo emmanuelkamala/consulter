a9e248df3ae0110ad6442c4d5ca5b4616090e864
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-09-08 00:10:08 +0300

    Configure application.rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::API
+  protect_from_forgery unless: -> { request.format.json? }
   include Knock::Authenticable
 end
--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,5 @@ module Consulter
     config.api_only = true
 
     config.load_defaults 6.0 and config.autoloader = :classic
-    config.action_controller.default_protect_from_forgery = true
   end
 end
