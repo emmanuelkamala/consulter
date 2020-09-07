5ce2d3729beb77f3ff5fdfe99fc46e88cf6f600c
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-09-07 15:32:27 +0300

    Edit Rubocop file errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe User, :type => :model do
   describe 'attributes' do
     it 'is valid with valid attributes' do
-      user = User.new(email: 'ejoka@hotmail.com', username: 'ejoka', password_digest: 'tanzania')
+      user = User.new(email: 'ejoka@hotmail.com', username: 'ejoka', password: 'tanzania')
       expect(user).to be_valid
     end
 
     it 'is valid with valid email' do
-      user = User.new(email: 'ejoka@hotmail.com', username: 'ejoka' password_digest: 'tanzania')
+      user = User.new(email: 'ejoka@hotmail.com', username: 'ejoka' password: 'tanzania')
       user.email = 'ejoka'
       expect(user.email).to_not be true
     end
